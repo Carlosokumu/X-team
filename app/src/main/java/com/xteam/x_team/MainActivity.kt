f0639caedcb8529816f13826d20262e9a012a569
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-11-11 00:22:20 +0300

    Databinding: MainActivity

--- app/src/main/java/com/xteam/x_team/MainActivity.kt
+++ app/src/main/java/com/xteam/x_team/MainActivity.kt
@@ -4,19 +4,24 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.xteam.x_team.databinding.ActivityMainBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 import com.xteam.x_team.MainViewModel as MainViewModel1
 
 class MainActivity : AppCompatActivity(), OnLinkClicked {
     private val viewModel by viewModel<MainViewModel1>()
     private lateinit var customSwitcher: CustomView
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         hideBar()
-        setContentView(R.layout.activity_main)
+      //  setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         val recyclerView=findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter=BlogAdapter(this)
         recyclerView.layoutManager=LinearLayoutManager(this)
